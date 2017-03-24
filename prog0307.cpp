293ea6a3fbe53daabaa9b17ec63e6d5f5325bc21
wagner <wagnerrodrigo.pan@gmail.com>
2017-03-24 11:58:04 -0300

    titulo do exercicio

--- prog0307.cpp
+++ prog0307.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+//Escreva um programa que calcule o salario bruto, salario liquido 
+//e os impostos a pagar seguido a regra
+// salario <1000  taxa 5%
+//salario >= 1000 e <5000 taxa 11%
+//salario >= 5000 taxa 35%
 int main(){
 	float salario,taxa;
 	printf("O seu Salario e: ");
